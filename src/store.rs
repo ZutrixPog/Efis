a884d807c145610202d7213b5a04c1665e288d17
erfan <erfansafari111@gmail.com>
2023-05-24 16:56:44 +0330

    slight data store mods

--- src/store.rs
+++ src/store.rs
@@ -1,8 +1,22 @@
 use std::time::{Instant, Duration};
+use std::sync::{Arc, Mutex};
+use std::collections::{VecDeque, HashSet, BTreeSet, HashMap};
+use thiserror::Error; // 1.0.40
 
 pub type Key = String;
 
+#[derive(Error, Debug)]
+pub enum DatastoreError {
+    #[error("Key doesnt exists")]
+    KeyNotFound,
+    #[error("Key-value is expired")]
+    KeyExpired,
+    #[error("There is something wrong")]
+    Other(String),
+}
+
 // TODO: It's better wrap theses but since they are std data structures, They won't cause us any trouble.
+#[derive(Debug)]
 pub enum Value {
     Text(String),
     Number(f64),
@@ -11,19 +25,19 @@ pub enum Value {
     SortedSet(BTreeSet<Value>),
 }
 
-struct Item<T> {
-    value: T,
+struct Item {
+    value: Value,
     expiry: Option<Instant>,
 }
 
 pub trait Datastore {
     type Type;
 
-    fn set(&mut self, key: String, value: Self::Value, expire_duration: Option<Duration>) -> Result<(), DatastoreError>;
+    fn set(&mut self, key: String, value: Self::Type, expire_duration: Option<Duration>) -> Result<(), DatastoreError>;
 
-    fn get(&self, key: &str) -> Option<&Self::Value>;
+    fn get(&self, key: &str) -> Option<&Self::Type>;
     
-    fn remove(&mut self, key: &str) -> Result<Self::Value, DatastoreError>;
+    fn remove(&mut self, key: &str) -> Result<Self::Type, DatastoreError>;
     
     fn expire(&mut self, key: &str, expire_duration: Duration) -> Result<(), DatastoreError>;
     
@@ -31,13 +45,13 @@ pub trait Datastore {
 }
 
 pub struct MemoryDataStore {
-    data: HashMap<Key, Item>,
+    data: Arc<Mutex<HashMap<Key, Item>>>,
 }
 
 impl MemoryDataStore {
     pub fn new() -> Self {
         Self {
-            data: HashMap::new(),
+            data: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 }
@@ -45,7 +59,7 @@ impl MemoryDataStore {
 impl Datastore for MemoryDataStore {
     type Type = Value;
 
-    fn set(&mut self, key: String, value: Type, expiry: Option<Duration>) {
+    fn set(&mut self, key: String, value: Self::Type, expiry: Option<Duration>) -> Result<(), DatastoreError> {
         let item = match expiry {
             Some(duration) => Item {
                 value,
@@ -56,45 +70,110 @@ impl Datastore for MemoryDataStore {
                 expiry: None,
             },
         };
-        self.data.insert(key, item);
+
+        let mut data = self.data.lock().unwrap();
+        data.insert(key, item);
+        Ok(())
     }
-    
-    fn get(&self, key: &str) -> Option<&Type> {
-        if let Some(item) = self.data.get(key) {
-            if let Some(expiry) = item.expiry {
-                if expiry <= Instant::now() {
-                    self.data.remove(key);
-                    return None;
-                }
+
+    fn get(&self, key: &str) -> Option<&Self::Type> {
+        let mut data = self.data.lock().unwrap();
+        if let Ok(Some(expiry)) = self.ttl(key) {
+            if expiry.is_zero() {
+                data.remove(key);
+                return None;
             }
-            Some(&item.value)
+            Some(&(data.get(key).unwrap().value.clone()))
         } else {
             None
         }
     }
-    
-    fn remove(&mut self, key: &str) -> Result<Type, DatastoreError> {
-        if self.data.remove(key).is_some() {
-            Ok(())
+
+    fn remove(&mut self, key: &str) -> Result<Self::Type, DatastoreError> {
+        let mut data = self.data.lock().unwrap();
+        if let Some(item) = data.remove(key) {
+            Ok(item.value)
         } else {
-            Err(DatastoreError::Other("couldn't delete key value"))
+            Err(DatastoreError::Other("couldn't delete key value".to_string()))
         }
     }
-    
+
     fn expire(&mut self, key: &str, duration: Duration) -> Result<(), DatastoreError> {
-        if let Some(item) = self.data.get_mut(key) {
+        let mut data = self.data.lock().unwrap();
+        if let Some(item) = data.get_mut(key) {
             item.expiry = Some(Instant::now() + duration);
             Ok(())
         } else {
-            Err(KeyNotFound)
+            Err(DatastoreError::KeyNotFound)
         }
     }
 
     fn ttl(&self, key: &str) -> Result<Option<Duration>, DatastoreError> {
-        if let Some(item) = self.data.get(key) {
-            Ok(item.expiry)
+        let data = self.data.lock().unwrap();
+        if let Some(item) = data.get(key) {
+            if let Some(exp) = item.expiry {
+                Ok(Some(exp - Instant::now()))
+            } else {
+                Ok(None)
+            }
         } else {
-            Err(KeyNotFound)
+            Err(DatastoreError::KeyNotFound)
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_and_get() {
+        let mut data_store = MemoryDataStore::new();
+
+        data_store.set("key1".to_owned(), Value::Text("value1".to_owned()), None);
+        data_store.set("key2".to_owned(), Value::Text("value2".to_owned()), Some(Duration::from_secs(10)));
+        data_store.set("key3".to_owned(), Value::Number(34.3), None);
+        data_store.set("key4".to_owned(), Value::List(VecDeque::new()), None);
+        data_store.set("key5".to_owned(), Value::Set(HashSet::new()), Some(Duration::from_secs(43)));
+        data_store.set("Key6".to_owned(), Value::SortedSet(BTreeSet::new()), None);
+
+        assert_eq!(data_store.get("key1"), Some(&Value::Text("value1".to_owned())));
+        assert_eq!(data_store.get("key2"), Some(&Value::Text("value2".to_owned())));
+        assert_eq!(data_store.get("non_existent_key"), None);
+        assert_eq!(data_store.get("key3"), Some(&Value::Number(34.3)));
+        assert_eq!(data_store.get("key4"), Some(&Value::List(VecDeque::new())));
+        // assert_eq!(data_store.get("key5"), Some(&Value::Text("value2".to_owned())));
+        // assert_eq!(data_store.get("key6"), Some(&Value::Text("value2".to_owned())));
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut data_store = MemoryDataStore::new();
+
+        data_store.set("key1".to_owned(), "value1".to_owned(), None);
+        data_store.set("key2".to_owned(), "value2".to_owned(), None);
+
+        let result = data_store.remove("key1");
+        assert!(result.is_ok());
+
+        let result = data_store.remove("non_existent_key");
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_expire_and_ttl() {
+        let mut data_store = MemoryDataStore::new();
+
+        data_store.set("key1".to_owned(), "value1".to_owned(), Some(Duration::from_secs(1)));
+        data_store.set("key2".to_owned(), "value2".to_owned(), None);
+
+        assert_eq!(data_store.ttl("key1"), Ok(Some(Duration::from_secs(1))));
+        assert_eq!(data_store.ttl("key2"), Ok(None));
+
+        std::thread::sleep(Duration::from_secs(2));
+
+        assert_eq!(data_store.ttl("key1"), Err(DatastoreError::KeyNotFound));
+        assert_eq!(data_store.get("key1"), None);
     }
 }
